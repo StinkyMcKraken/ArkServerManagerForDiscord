b2c7de394dbbf13b3dc43ee025f4dc0d8571bc0b
John Kapuscinski <stinkymckraken@gmail.com>
2021-02-03 08:41:23 -0800

    tweak comments on notification scripts

--- sendupdatenotice.sh
+++ sendupdatenotice.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#expecting command line argument providing the name of
-#the server to operate on
+# expecting command line argument providing the name of
+# the server to operate on
 selfname="${1}"
 
-#go get our variables
+# go get our variables
 source "/home/ark/scripts/globals.sh"
-#lead time before shutting down the server in minutes
+# lead time before shutting down the server in minutes
 # figure out how to test for time variable.  if unset, then give it a default
+# default time specified in globals.sh
 
 message="Broadcast Server will Shutdown for update in"
 
-#run notifications every minute until less than a minute
+# run notifications every minute until less than a minute
 while (( ${time} > 1 ))
 do
 	${rcon} "${message} ${time} minutes"
@@ -21,7 +22,7 @@ done
 
 ${rcon} "serverchat Server will shutdown to perform updates. Server will be available in about 10 minutes"
 
-#notifications every 10 seconds in the last minute
+# notifications every 10 seconds in the last minute
 time=60
 while (( ${time} > 10 ))
 do
@@ -30,7 +31,7 @@ do
 	time=$(( ${time} - 10 ))
 done
 
-#notifications every second in the last 10 seconds
+# notifications every second in the last 10 seconds
 while (( ${time} > 0 ))
 do
 	${rcon} "${message} ${time} seconds"
@@ -38,5 +39,5 @@ do
 	time=$(( ${time} - 1 ))
 done
 
-#final notification
+# final notification
 ${rcon} "Broadcast Bansai!!"
