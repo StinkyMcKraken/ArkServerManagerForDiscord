66620a92eb273347f4ff0ad0afd48d5ac9df65b5
John Kapuscinski <stinkymckraken@gmail.com>
2021-02-08 07:08:23 -0800

    add updatelgsm script

---
+++ updatelgsm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# update lgsm on multiple instances by running lgsm update command on
+# generic arkserver then copying generic akrserver script over to other
+# instances
+
+/home/ark/arkserver ul
+cp /home/ark/arkserver /home/ark/island
+cp /home/ark/arkserver /home/ark/aberration
+cp /home/ark/arkserver /home/ark/ragnarok
+cp /home/ark/arkserver /home/ark/scorched
+cp /home/ark/arkserver /home/ark/center
+cp /home/ark/arkserver /home/ark/crystal
+cp /home/ark/arkserver /home/ark/extinction
+cp /home/ark/arkserver /home/ark/valguero
+cp /home/ark/arkserver /home/ark/genesis
+cp /home/ark/arkserver /home/ark/genesis2
+cp /home/ark/arkserver /home/ark/arkadmin
